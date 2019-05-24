1effdb68f5df5163397a61fa52365432d8eae516
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-05-24 22:05:46 +0700

    Add new class dog and mouse, add stomach method for cat class

--- cat.js
+++ cat.js
@@ -1,10 +1,15 @@
 function cat(name){
     this.name=name;
     this.dead=false;
+    this.stomach=[];
 }
 
 cat.prototype.die=function(){
     this.dead=true;
+}
+
+cat.prototype.eat(mouse)=function(){
+    this.stomach.push(mouse);
 }
 
 module.export=cat;
