51285154537f7e11cc8668c5a97daccfa3d5ffcb
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-05-25 22:24:01 +0700

    Fixed file cat.js

--- cat.js
+++ cat.js
@@ -1,12 +1,8 @@
 function cat(name){
-<<<<<<< HEAD
+
     this.name=name;
     this.dead=false;
     this.stomach=[];
-}
-=======
->>>>>>> parent of dcee2da... Add die method for cat
-
 }
 
 cat.prototype.eat(mouse)=function(){
